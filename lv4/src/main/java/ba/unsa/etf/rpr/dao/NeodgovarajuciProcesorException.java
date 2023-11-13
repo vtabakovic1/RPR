11e62c861a59f1e1f4d12823bfed3725759b9fd9
vtabakovic1 <vtabakovic1@etf.unsa.ba>
2023-11-13 16:08:08 +0100

    ...

---
+++ lv4/src/main/java/ba/unsa/etf/rpr/dao/NeodgovarajuciProcesorException.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.dao;
+
+public class NeodgovarajuciProcesorException extends RuntimeException{
+    public NeodgovarajuciProcesorException(String poruka){
+        super(poruka);
+    }
+}
