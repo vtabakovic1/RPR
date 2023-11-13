6f59f7bafa55b73083e1527da9d7f9057bac1a90
vtabakovic1 <vtabakovic1@etf.unsa.ba>
2023-11-13 16:12:57 +0100

    ...

--- lv4/src - Copy/main/java/ba/unsa/etf/rpr/App.java
+++
@@ -1,6 +0,0 @@
-package ba.unsa.etf.rpr;
-
-public class App {
-    public static void main(String[] args) {
-    }
-}
--- lv4/src - Copy/main/java/ba/unsa/etf/rpr/dao/NeodgovarajuciProcesorException.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.rpr.dao;
-
-public class NeodgovarajuciProcesorException extends RuntimeException{
-    public NeodgovarajuciProcesorException(String poruka){
-        super(poruka);
-    }
-}
